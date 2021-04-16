7828269b135e3debda8cb35e115bc7ccea7d3561
chamedm <is708177@iteso.mx>
2021-04-15 20:39:36 -0500

    Renaming files and labels

--- lib/home/noticias_ext_api/listado_noticias.dart
+++ lib/home/noticias_ext_api/listado_noticias.dart
@@ -3,8 +3,8 @@ import 'package:google_login/utils/news_repository.dart';
 
 import 'item_noticia.dart';
 
-class NoticiasDeportes extends StatelessWidget {
-  const NoticiasDeportes({Key key}) : super(key: key);
+class ListadoNoticias extends StatelessWidget {
+  const ListadoNoticias({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
